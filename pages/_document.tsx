79d1ba0b96d604412904d0df736e6bd8a1916bcc
appear <appear.ko@gmail.com>
2020-01-17 16:44:00 +0900

    :zap: _document 추가

---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Document, {
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentProps
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+interface MyDocumentProps extends DocumentProps {
+  styleTags?: string;
+}
+
+function MyDocument({ styleTags }: MyDocumentProps) {
+  return (
+    <html>
+      <Head>{styleTags}</Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </html>
+  );
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const initialProps = await Document.getInitialProps(ctx);
+  /* pre render styled component */
+  const sheet = new ServerStyleSheet();
+  const page = ctx.renderPage(App => props =>
+    sheet.collectStyles(
+      <>
+        <App {...props} />
+      </>
+    )
+  );
+  const styleTags = sheet.getStyleElement();
+
+  return { ...initialProps, ...page, styleTags };
+};
+
+export default MyDocument;
