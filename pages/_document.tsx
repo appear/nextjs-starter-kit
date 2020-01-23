88758c8ffb603c555bc2a855d2e7bf6a83043c92
appear <appear.ko@gmail.com>
2020-01-23 19:23:42 +0900

    :robot: fix _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,32 +12,32 @@ interface MyDocumentProps extends DocumentProps {
   styleTags?: string
 }
 
-function MyDocument({ styleTags }: MyDocumentProps) {
-  return (
-    <html>
-      <Head>{styleTags}</Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </html>
-  )
+export default class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    /* pre render styled component */
+    const sheet = new ServerStyleSheet()
+    const page = ctx.renderPage((App) => (props) =>
+      sheet.collectStyles(
+        <>
+          <App {...props} />
+        </>,
+      ),
+    )
+    const styleTags = sheet.getStyleElement()
+
+    return { ...initialProps, ...page, styleTags }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
 }
-
-MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const initialProps = await Document.getInitialProps(ctx)
-  /* pre render styled component */
-  const sheet = new ServerStyleSheet()
-  const page = ctx.renderPage((App) => (props) =>
-    sheet.collectStyles(
-      <>
-        <App {...props} />
-      </>,
-    ),
-  )
-  const styleTags = sheet.getStyleElement()
-
-  return { ...initialProps, ...page, styleTags }
-}
-
-export default MyDocument
